225e6811a06b4f91cdac4f3232d1a92440e45583
Albert <albertonly898@gmail.com>
2019-04-16 22:05:28 +0700

    First Commit

---
+++ src/components/elements/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loader">
+      <p />
+    </div>
+  );
+};
+
+export default Spinner;
