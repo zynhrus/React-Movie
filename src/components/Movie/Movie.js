be4decc088854b12c9ed878487c7830b7e931779
Albert <albertonly898@gmail.com>
2019-04-17 20:40:23 +0700

    Router

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Movie extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Movie;
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This page doesn't exist</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
